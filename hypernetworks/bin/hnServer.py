ddba990225f549d1abd90a97e62d534812e370e8
Richard Charlesworth <richard@rcharlie.me.uk>
2020-12-04 16:28:07 +0000

    Added wsgi capability to hnServer

--- hypernetworks/bin/hnServer.py
+++ hypernetworks/bin/hnServer.py
@@ -194,4 +194,4 @@ def subhn():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from hypernetworks.bin.hnServer import app
+
+if __name__ == "__main__":
+    app.run()
