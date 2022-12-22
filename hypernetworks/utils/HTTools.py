6f8a094abb86aa8463e9d259145f63790f5faf49
Richard Charlesworth <richard@rcharlie.me.uk>
2022-12-22 13:54:17 +0000

    Added get sub-Hs by level

--- hypernetworks/utils/HTTools.py
+++ hypernetworks/utils/HTTools.py
@@ -178,6 +178,56 @@ def get_subHn_by_semantic_boundary(hn, boundary, subHn):
     return subHn
 
 
+def get_subHn_by_level(hn, level, subHn):
+    def _insert(_name, _hs):
+        subHn.insert(vertex=_name, hs_class=_hs.hs_class, hstype=_hs.hstype, simplex=_hs.simplex,
+                     R=_hs.R, t=_hs.t, C=_hs.C, B=_hs.B, N=_hs.N,
+                     psi=_hs.psi, psi_inv=_hs.psi_inv, phi=_hs.phi, phi_inv=_hs.phi_inv,
+                     traffic=_hs.traffic, coloured=_hs.coloured, boundary_exclusions=hn._boundary_exclusions)
+
+        temp_phis.add(_hs.phi)
+        temp_psis.add(_hs.psi)
+
+    # if not subHn:
+    #     subHn = Hypernetwork()
+
+    temp_phis = set()
+    temp_phi_invs = set()
+    temp_psis = set()
+    temp_psi_invs = set()
+    temp_relations = set()
+
+    for name, hs in hn.hypernetwork.items():
+        if level == hs.N:
+            print(level, hs.N, name, hs)
+            _insert(name, hs)
+            # for vertex in hs.simplex:
+            #     v_hs = hn.hypernetwork[vertex]
+            #     _insert(vertex, v_hs)
+
+    for phi in temp_phis:
+        if phi:
+            subHn.phis[phi] = hn.phis[phi]
+
+    for phi_inv in temp_phi_invs:
+        if phi_inv:
+            subHn.phis[phi_inv] = hn.phis[phi_inv]
+
+    for psi in temp_psis:
+        if psi:
+            subHn.phis[psi] = hn.phis[psi]
+
+    for psi_inv in temp_psi_invs:
+        if psi_inv:
+            subHn.phis[psi_inv] = hn.phis[psi_inv]
+
+    for rel in temp_relations:
+        if rel:
+            subHn.relations[rel] = hn.relations[rel]
+
+    return subHn
+
+
 def add_phis(hn):
     phis = set()
     for vector, hs in hn.hypernetwork.items():
